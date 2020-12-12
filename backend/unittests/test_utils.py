6b08c8b784e352cc27c65b02b7f65e901aa93bbd
Alexander Batashev <alexbatashev@outlook.com>
2020-12-12 10:44:08 +0300

    fix test formatting

--- backend/unittests/test_utils.py
+++ backend/unittests/test_utils.py
@@ -1,11 +1,12 @@
 from app import utils
 import unittest
 
-class TestUtils(unittest.TestCase):
-  def test_get_film_id(self):
-    res = utils.get_film_id('t01')
-    self.assertEqual(len(res), 10)
 
-  def test_get_film_id(self):
-    res = utils.get_person_id('nm0001')
-    self.assertEqual(len(res), 14)
+class TestUtils(unittest.TestCase):
+    def test_get_film_id(self):
+        res = utils.get_film_id('t01')
+        self.assertEqual(len(res), 10)
+
+    def test_get_film_id(self):
+        res = utils.get_person_id('nm0001')
+        self.assertEqual(len(res), 14)
