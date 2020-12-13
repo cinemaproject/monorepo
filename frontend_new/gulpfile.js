de55fe54e184bec1854552521930f1c695b53988
Alexander Batashev <alexbatashev@outlook.com>
2020-12-13 10:40:51 +0300

    Add serve task

--- frontend_new/gulpfile.js
+++ frontend_new/gulpfile.js
@@ -4,26 +4,32 @@ const concatCss = require('gulp-concat-css');
 const copy = require('gulp-copy');
 const browserSync = require('browser-sync').create();
 
-function render_templates(cb) {
-  src('views/**/*.js', { buffer: false })
+function render_templates() {
+  return src('views/**/*.js', { buffer: false })
     .pipe(injectTemplate({variable: 'parameters'}))
     .pipe(dest('./build/rendered_views/'));
-  cb();
 }
 
-function compile_css(cb) {
-  src('styles/**/*.css')
+function compile_css() {
+  return src('styles/**/*.css')
     .pipe(concatCss("main.css"))
     .pipe(dest('./build/styles'));
-  cb();
 }
 
-function deploy(cb) {
-  src(['./build/rendered_views/*.js']).pipe(copy('./dist/static/js/views/', { prefix: 2 }));
-  src('./styles/main.css').pipe(copy('./dist/static/css/', { prefix: 1 }));
-  src('./app/main.js').pipe(copy('./dist/static/js/', { prefix: 1 }));
-  src('./index.html').pipe(copy('./dist/'));
-  cb();
+function deployViewJS() {
+  return src(['./build/rendered_views/*.js']).pipe(copy('./dist/static/js/views/', { prefix: 2 }));
+}
+
+function deployCSS() {
+  return src('./styles/main.css').pipe(copy('./dist/static/css/', { prefix: 1 }));
+}
+
+function deployJS() {
+  return src('./app/main.js').pipe(copy('./dist/static/js/', { prefix: 1 }));
+}
+
+function deployHTML() {
+  return src('./index.html').pipe(copy('./dist/'));
 }
 
 function serve(cb) {
@@ -32,11 +38,19 @@ function serve(cb) {
       baseDir: "./dist"
     }
   });
-  //cb();
 }
 
-exports.default = series(
-  parallel(render_templates, compile_css),
-  deploy,
-  serve
+const cssPipeline = series(compile_css);
+const jsPipeline = series(render_templates);
+const deploy = series(deployCSS, deployJS, deployViewJS, deployHTML);
+
+const defaultPipeline = series(
+  parallel(cssPipeline, jsPipeline),
+  deploy
 );
+
+exports.default = defaultPipeline;
+exports.serve = series(
+  defaultPipeline,
+  serve
+); 
