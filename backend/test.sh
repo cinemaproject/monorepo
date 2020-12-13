9b5d397b3d598222ab63565009bfce1e4d586858
Alexander Batashev <alexbatashev@outlook.com>
2020-12-13 20:42:28 +0300

    chore: add code quality metrics (#8)

--- backend/test.sh
+++ backend/test.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
-pytest unittests --doctest-modules --junitxml=junit/test-results.xml --cov=app --cov-report=xml --cov-report=html
+pytest unittests --doctest-modules --junitxml=junit/test-results.xml --cov=app --cov-report=xml --cov-report=html --cov-report=term
--- backend/unittests/test_utils.py
+++ backend/unittests/test_utils.py
@@ -2,10 +2,10 @@ from app import utils
 
 
 def test_get_film_id():
-        res = utils.get_film_id('t01')
-        assert len(res) == 10
+    res = utils.get_film_id("t01")
+    assert len(res) == 10
 
 
 def test_get_person_id():
-        res = utils.get_person_id('nm01')
-        assert len(res) == 14
+    res = utils.get_person_id("nm01")
+    assert len(res) == 14
