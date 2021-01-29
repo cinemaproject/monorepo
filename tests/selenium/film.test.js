22c513006093a6a220575de9adcade00e8295133
Alexander Batashev <alexbatashev@outlook.com>
2021-01-29 21:00:52 +0300

    more updates

--- tests/selenium/film.test.js
+++ tests/selenium/film.test.js
@@ -7,13 +7,13 @@ afterAll(async () => await browser.quit());
 
 describe('Film Page', () => {
   test('it should have a film id', async () => {
-    await browser.get(base_url + "/#/film/t000000001");
+    await browser.get(base_url + "/#/film/1");
     browser.wait(until.elementLocated(By.id('film-id'), 100000));
     const id = await browser.findElement(By.id('film-id')).getText()
-    expect(id.trim()).toBe('id: t000000001');
+    expect(id.trim()).toBe('id: 1');
   });
   test('it should have a title', async () => {
-    await browser.get(base_url + "/#/film/t000000001");
+    await browser.get(base_url + "/#/film/1");
     const title = await browser.findElement(By.id('film-title')).getText()
     expect(title.trim()).toBe('title: Sample Film');
   });
