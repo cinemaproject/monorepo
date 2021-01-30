5242bb4cd4c9fd37e5f71019c9e16ced44d9a656
Alexander Batashev <alexbatashev@outlook.com>
2021-01-30 11:02:04 +0300

    put missing awaits

--- tests/selenium/film.test.js
+++ tests/selenium/film.test.js
@@ -11,13 +11,13 @@ afterAll(async () => await browser.quit());
 describe('Film Page', () => {
   test('it should have a film id', async () => {
     await browser.get(base_url + "/#/film/1");
-    browser.wait(until.elementLocated(By.id('film-id'), 300000));
+    await browser.wait(until.elementLocated(By.id('film-id'), 500));
     const id = await browser.findElement(By.id('film-id')).getText()
     expect(id.trim()).toBe('id: 1');
   });
   test('it should have a title', async () => {
     await browser.get(base_url + "/#/film/1");
-    browser.wait(until.elementLocated(By.id('film-title'), 300000));
+    await browser.wait(until.elementLocated(By.id('film-title'), 500));
     const title = await browser.findElement(By.id('film-title')).getText()
     expect(title.trim()).toBe('title: Sample Film');
   });
